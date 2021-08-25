a5554ec52d214a739e38edf3fdb4f31b86656eb3
daybreak-e <scja1704@naver.com>
2021-08-25 15:31:53 +0900

    MemoList update

---
+++ MemoApp/MemoApp/UIViewController+Alert.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewController+Alert.swift
+//  MemoApp
+//
+//  Created by 이새벽 on 2021/08/25.
+//
+
+import UIKit
+
+extension UIViewController {
+    func alert(title: String = "알림", message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        alert.addAction(okAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+}
